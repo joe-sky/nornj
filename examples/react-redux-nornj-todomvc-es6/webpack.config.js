555216f8d720681d8ec517e4b92fa9af0d394e55
joe-sky <hitomi_doax@sina.com>
2016-03-15 19:12:11 +0800

    update webpack.config

--- examples/react-redux-nornj-todomvc-es6/webpack.config.js
+++ examples/react-redux-nornj-todomvc-es6/webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      'process.env.NODE_ENV': '"production"'
     }),
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
