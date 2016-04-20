741eb4c07b18e6d3f2fd1a1d7b92c0b1d37f33b3
joe-sky <hitomi_doax@sina.com>
2016-04-20 16:10:59 +0800

    Fix precompiler's bug

--- precompiler.js
+++ precompiler.js
@@ -15,8 +15,10 @@ module.exports = function (param) {
     glob.sync(source, param.options).forEach(function (file) {
       var name = file.substr(0, file.lastIndexOf('.')),
         newName = name.substr(0, name.lastIndexOf('.')) + '.js',
-        preTmpl,
-        tmpl = require(name);  //Load original template
+        preTmpl;
+        
+      delete require.cache[require.resolve(name)];  //First clear the module cache
+      var tmpl = require(name);  //Load original template
 
       preTmpl = param.esVersion === 'es6' ? 'export default ' : 'module.exports = ';
       preTmpl += JSON.stringify(precompile(tmpl));  //Precompile template
