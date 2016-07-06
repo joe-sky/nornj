72718c0e5f5576663f422c6bd3f2eb9276f79604
joe-sky <hitomi_doax@sina.com>
2016-07-06 23:48:39 +0800

    Fix code indent

--- examples/backbone-marionette-nornj-todomvc-es5/js/TodoMVC.Layout.js
+++ examples/backbone-marionette-nornj-todomvc-es5/js/TodoMVC.Layout.js
@@ -3,124 +3,124 @@
 var TodoMVC = TodoMVC || {};
 
 (function () {
-	'use strict';
+  'use strict';
 
-	var filterChannel = Backbone.Radio.channel('filter');
+  var filterChannel = Backbone.Radio.channel('filter');
 
-	TodoMVC.RootLayout = Backbone.Marionette.LayoutView.extend({
+  TodoMVC.RootLayout = Backbone.Marionette.LayoutView.extend({
 
-		el: '#todoapp',
+    el: '#todoapp',
 
-		regions: {
-			header: '#header',
-			main: '#main',
-			footer: '#footer'
-		}
-	});
+    regions: {
+      header: '#header',
+      main: '#main',
+      footer: '#footer'
+    }
+  });
 
-	// Layout Header View
-	// ------------------
-	TodoMVC.HeaderLayout = Backbone.Marionette.ItemView.extend({
+  // Layout Header View
+  // ------------------
+  TodoMVC.HeaderLayout = Backbone.Marionette.ItemView.extend({
 
-		// template: '#template-header',
-		template: nj.compile(document.getElementById('template-header').innerHTML),
+    // template: '#template-header',
+    template: nj.compile(document.getElementById('template-header').innerHTML),
 
-		// UI bindings create cached attributes that
-		// point to jQuery selected objects
-		ui: {
-			input: '#new-todo'
-		},
+    // UI bindings create cached attributes that
+    // point to jQuery selected objects
+    ui: {
+      input: '#new-todo'
+    },
 
-		events: {
-			'keypress @ui.input': 'onInputKeypress',
-			'keyup @ui.input': 'onInputKeyup'
-		},
+    events: {
+      'keypress @ui.input': 'onInputKeypress',
+      'keyup @ui.input': 'onInputKeyup'
+    },
 
-		// According to the spec
-		// If escape is pressed during the edit, the edit state should be left and any changes be discarded.
-		onInputKeyup: function (e) {
-			var ESC_KEY = 27;
+    // According to the spec
+    // If escape is pressed during the edit, the edit state should be left and any changes be discarded.
+    onInputKeyup: function (e) {
+      var ESC_KEY = 27;
 
-			if (e.which === ESC_KEY) {
-				this.render();
-			}
-		},
+      if (e.which === ESC_KEY) {
+        this.render();
+      }
+    },
 
-		onInputKeypress: function (e) {
-			var ENTER_KEY = 13;
-			var todoText = this.ui.input.val().trim();
+    onInputKeypress: function (e) {
+      var ENTER_KEY = 13;
+      var todoText = this.ui.input.val().trim();
 
-			if (e.which === ENTER_KEY && todoText) {
-				this.collection.create({
-					title: todoText
-				});
-				this.ui.input.val('');
-			}
-		}
-	});
+      if (e.which === ENTER_KEY && todoText) {
+        this.collection.create({
+          title: todoText
+        });
+        this.ui.input.val('');
+      }
+    }
+  });
 
-	// Layout Footer View
-	// ------------------
-	TodoMVC.FooterLayout = Backbone.Marionette.ItemView.extend({
-		// template: '#template-footer',
-		template: nj.compile(nj(document.getElementById('template-footer').innerHTML, ' ', ' ')),
+  // Layout Footer View
+  // ------------------
+  TodoMVC.FooterLayout = Backbone.Marionette.ItemView.extend({
+    // template: '#template-footer',
+    template: nj.compile(nj(document.getElementById('template-footer').innerHTML, ' ', ' ')),
 
-		// UI bindings create cached attributes that
-		// point to jQuery selected objects
-		ui: {
-			filters: '#filters a',
-			completed: '.completed a',
-			active: '.active a',
-			all: '.all a',
-			summary: '#todo-count',
-			clear: '#clear-completed'
-		},
+    // UI bindings create cached attributes that
+    // point to jQuery selected objects
+    ui: {
+      filters: '#filters a',
+      completed: '.completed a',
+      active: '.active a',
+      all: '.all a',
+      summary: '#todo-count',
+      clear: '#clear-completed'
+    },
 
-		events: {
-			'click @ui.clear': 'onClearClick'
-		},
+    events: {
+      'click @ui.clear': 'onClearClick'
+    },
 
-		collectionEvents: {
-			all: 'render'
-		},
+    collectionEvents: {
+      all: 'render'
+    },
 
-		templateHelpers: {
-			activeCountLabel: function () {
-				return (this.activeCount === 1 ? 'item' : 'items') + ' left';
-			}
-		},
+    templateHelpers: {
+      activeCountLabel: function () {
+        return (this.activeCount === 1 ? 'item' : 'items') + ' left';
+      }
+    },
 
-		initialize: function () {
-			this.listenTo(filterChannel.request('filterState'), 'change:filter', this.updateFilterSelection, this);
-		},
+    initialize: function () {
+      this.listenTo(filterChannel.request('filterState'), 'change:filter', this.updateFilterSelection, this);
+    },
 
-		serializeData: function () {
-			var active = this.collection.getActive().length;
-			var total = this.collection.length;
+    serializeData: function () {
+      var active = this.collection.getActive().length;
+      var total = this.collection.length;
 
-			return {
-				activeCount: active,
-				totalCount: total,
-				completedCount: total - active
-			};
-		},
+      return {
+        activeCount: active,
+        totalCount: total,
+        completedCount: total - active
+      };
+    },
 
-		onRender: function () {
-			this.$el.parent().toggle(this.collection.length > 0);
-			this.updateFilterSelection();
-		},
+    onRender: function () {
+      this.$el.parent().toggle(this.collection.length > 0);
+      this.updateFilterSelection();
+    },
 
-		updateFilterSelection: function () {
-			this.ui.filters.removeClass('selected');
-			this.ui[filterChannel.request('filterState').get('filter')]
+    updateFilterSelection: function () {
+      this.ui.filters.removeClass('selected');
+      this.ui[filterChannel.request('filterState').get('filter')]
 			.addClass('selected');
-		},
+    },
 
-		onClearClick: function () {
-			var completed = this.collection.getCompleted();
-			completed.forEach(function (todo) {
-				todo.destroy();
-			});
-		}
-	});
+    onClearClick: function () {
+      var completed = this.collection.getCompleted();
+      completed.forEach(function (todo) {
+        todo.destroy();
+      });
+    }
+  });
 })();
--- examples/backbone-marionette-nornj-todomvc-es5/js/TodoMVC.Router.js
+++ examples/backbone-marionette-nornj-todomvc-es5/js/TodoMVC.Router.js
@@ -3,71 +3,71 @@
 var TodoMVC = TodoMVC || {};
 
 (function () {
-	'use strict';
+  'use strict';
 
-	var filterChannel = Backbone.Radio.channel('filter');
+  var filterChannel = Backbone.Radio.channel('filter');
 
-	// TodoList Router
-	// ---------------
-	//
-	// Handles a single dynamic route to show
-	// the active vs complete todo items
-	TodoMVC.Router = Backbone.Marionette.AppRouter.extend({
-		appRoutes: {
-			'*filter': 'filterItems'
-		}
-	});
+  // TodoList Router
+  // ---------------
+  //
+  // Handles a single dynamic route to show
+  // the active vs complete todo items
+  TodoMVC.Router = Backbone.Marionette.AppRouter.extend({
+    appRoutes: {
+      '*filter': 'filterItems'
+    }
+  });
 
-	// TodoList Controller (Mediator)
-	// ------------------------------
-	//
-	// Control the workflow and logic that exists at the application
-	// level, above the implementation detail of views and models
-	TodoMVC.Controller = Backbone.Marionette.Object.extend({
+  // TodoList Controller (Mediator)
+  // ------------------------------
+  //
+  // Control the workflow and logic that exists at the application
+  // level, above the implementation detail of views and models
+  TodoMVC.Controller = Backbone.Marionette.Object.extend({
 
-		initialize: function () {
-			this.todoList = new TodoMVC.TodoList();
-		},
+    initialize: function () {
+      this.todoList = new TodoMVC.TodoList();
+    },
 
-		// Start the app by showing the appropriate views
-		// and fetching the list of todo items, if there are any
-		start: function () {
-			this.showHeader(this.todoList);
-			this.showFooter(this.todoList);
-			this.showTodoList(this.todoList);
-			this.todoList.on('all', this.updateHiddenElements, this);
-			this.todoList.fetch();
-		},
+    // Start the app by showing the appropriate views
+    // and fetching the list of todo items, if there are any
+    start: function () {
+      this.showHeader(this.todoList);
+      this.showFooter(this.todoList);
+      this.showTodoList(this.todoList);
+      this.todoList.on('all', this.updateHiddenElements, this);
+      this.todoList.fetch();
+    },
 
-		updateHiddenElements: function () {
-			$('#main, #footer').toggle(!!this.todoList.length);
-		},
+    updateHiddenElements: function () {
+      $('#main, #footer').toggle(!!this.todoList.length);
+    },
 
-		showHeader: function (todoList) {
-			var header = new TodoMVC.HeaderLayout({
-				collection: todoList
-			});
-			TodoMVC.App.root.showChildView('header', header);
-		},
+    showHeader: function (todoList) {
+      var header = new TodoMVC.HeaderLayout({
+        collection: todoList
+      });
+      TodoMVC.App.root.showChildView('header', header);
+    },
 
-		showFooter: function (todoList) {
-			//TODO filter
-			var footer = new TodoMVC.FooterLayout({
-				collection: todoList
-			});
-			TodoMVC.App.root.showChildView('footer', footer);
-		},
+    showFooter: function (todoList) {
+      //TODO filter
+      var footer = new TodoMVC.FooterLayout({
+        collection: todoList
+      });
+      TodoMVC.App.root.showChildView('footer', footer);
+    },
 
-		showTodoList: function (todoList) {
-			TodoMVC.App.root.showChildView('main', new TodoMVC.ListView({
-				collection: todoList
-			}));
-		},
+    showTodoList: function (todoList) {
+      TodoMVC.App.root.showChildView('main', new TodoMVC.ListView({
+        collection: todoList
+      }));
+    },
 
-		// Set the filter to show complete or all items
-		filterItems: function (filter) {
-			var newFilter = filter && filter.trim() || 'all';
-			filterChannel.request('filterState').set('filter', newFilter);
-		}
-	});
+    // Set the filter to show complete or all items
+    filterItems: function (filter) {
+      var newFilter = filter && filter.trim() || 'all';
+      filterChannel.request('filterState').set('filter', newFilter);
+    }
+  });
 })();
