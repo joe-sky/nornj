6a30095a40e2e6f25b4f9499ee298b335735b49f
joe-sky <hitomi_doax@sina.com>
2016-12-02 00:14:12 +0800

    Updated test specs

--- test/compileStringSpec.js
+++ test/compileStringSpec.js
@@ -3,7 +3,7 @@
   compile = require('../src/compiler/compile').compile,
   includeParser = require('../tools/includeParser');
 
-xdescribe('test compile string', function () {
+describe('test compile string', function () {
   beforeAll(function () {
     nj.registerFilter('filter1', function (v, p1) {
       //console.log(this.useString);
@@ -48,7 +48,7 @@ xdescribe('test compile string', function () {
       };
 
       var tmpl1 = `
-        <div name1="../111" class="{ c1 } c0{ '5' | filter1 }{'!'}{ '10' | filter1('1') } c2" id1=666 id2=777 name="my name:{'name' name},id:{id},name:{name}" id=test1>
+        <div name1="../111" class="{ c1 } c0{ '5' | filter1 }{'!'}{ '10' | filter1('1') } c2" id1=666 id2=777 name="my name:{'name'}{name},id:{id},name:{name}" id=test1>
           <span>
             sky:{name},{id}
           </span>
