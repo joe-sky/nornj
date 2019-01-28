6ad4e7dc1a9cca236c5d97c5f9989d61ea0739bc
joe-sky <hitomi_doax@sina.com>
2019-01-28 19:36:56 +0800

    Update test spec

--- test/compiler/compile.spec.js
+++ test/compiler/compile.spec.js
@@ -21,19 +21,35 @@ describe('Precompile', () => {
 
   xit('Simple', () => {
     const tmpl = `
-      {{1 + 2 ** 3}}
+      {{1 + 2 ** 3 - 4 * 5 %% 6}}
       {{'111'.length + 2 * 3}}
       {{' 123 '.trim() + 4 - 5 ** 6}}
+      {{ { a: 1, b: 2 }.b * 100 }}
+      {{[1, 2, 3][0] + 100}}
+      {{'abcde'.substring(1, num.getLength()).length * 5}}
     `;
 
     const ret = precompile(tmpl, true, nj.tmplRule);
 
     const gCode = `
-      1 + p1.f['**'](2, 3)
+      1 + p1.f['**'](2, 3) - 4 * p1.f['%%'](5, 6)
+
       p1.f['.']('111', 'length') + 2 * 3
-      p1.f['.'](' 123 ', 'trim')() + 4 - p1.f['**'](5, 6)
+
+      var _v0 = p1.f['.'](' 123 ', 'trim');
+      (_v0 && _v0()) + 4 - p1.f['**'](5, 6)
+
+      p1.f['.']({ a: 1, b: 2 }, 'b') * 100
+
+      p1.f['.']([1, 2, 3], 0) + 100
+
+      var _v0 = p1.f['.']('abcde', 'substring');
+      var _v1 = p1.f['.'](p2.d('num'), 'getLength');
+      p1.f['.']((_v0 && _v0(1, (_v1 && _v1()))), 'length') * 5
     `;
 
-    console.log(ret.main.toString());
+    //console.log(ret.main.toString());
+    var _v0 = null;
+    console.log('1' + (_v0 && _v0()) + 3);
   });
 });
