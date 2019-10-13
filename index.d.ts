abba50e7058f96178f74bda29d0b63c85c0b45cd
joe-sky <hitomi_doax@sina.com>
2019-10-13 16:47:09 +0800

    Update typings

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,10 @@
 import './src/typings/global';
 import { NornJ } from './src/typings/nj';
 
-/**
- * NornJ tagged templates syntax `nj`(full name is `nj.taggedTmplH`), example:
- * 
- * `nj'<html>Hello World!</html>'()`
- */
-declare const NornJExport: NornJ;
-export = NornJExport;
+declare module 'nornj' {
+	export = NornJ;
+}
+
+declare module 'nornj/dist/nornj.common' {
+	export = NornJ;
+}
--- src/typings/nj.d.ts
+++ src/typings/nj.d.ts
@@ -4,113 +4,115 @@
 export interface NornJComponent { }
 
 /**
- * NornJ filter options.
+ * NornJ tagged templates syntax `nj`(full name is `nj.taggedTmplH`), example:
+ * 
+ * `nj'<html>Hello World!</html>'()`
  */
-export interface FilterOption {
-  onlyGlobal: boolean;
-  hasOptions: boolean;
-  isOperator: boolean;
-  hasLevel: boolean;
-  hasTmplCtx: boolean;
-  alias: string;
-  symbol: string;
-  placeholder: string;
-  [key: string]: any;
-}
-
-/**
- * NornJ filter configs.
- */
-export interface FilterConfig {
-  [key: string]: FilterOption;
-}
-
-/**
- * NornJ template engine.
- */
-export interface NornJ {
+declare namespace NornJ {
   /**
-   * `nj.registerComponent`, register component to NornJ.
+   * NornJ filter options.
    */
-  registerComponent(name: string, component: NornJComponent, options?: object): NornJComponent | NornJComponent[];
+  export interface FilterOption {
+    onlyGlobal: boolean;
+    hasOptions: boolean;
+    isOperator: boolean;
+    hasLevel: boolean;
+    hasTmplCtx: boolean;
+    alias: string;
+    symbol: string;
+    placeholder: string;
+    [key: string]: any;
+  }
+
+  /**
+   * NornJ filter configs.
+   */
+  export interface FilterConfig {
+    [key: string]: FilterOption;
+  }
 
   /**
    * `nj.registerComponent`, register component to NornJ.
    */
-  registerComponent(options: object): NornJComponent | NornJComponent[];
+  export function registerComponent(name: string, component: NornJComponent, options?: object): NornJComponent | NornJComponent[];
+
+  /**
+   * `nj.registerComponent`, register component to NornJ.
+   */
+  export function registerComponent(options: object): NornJComponent | NornJComponent[];
 
   /**
    * `nj.registerFilter`, register filter and expression to NornJ.
    */
-  registerFilter(name: string, filter: Function, options?: FilterOption, mergeConfig?: boolean): void;
+  export function registerFilter(name: string, filter: Function, options?: FilterOption, mergeConfig?: boolean): void;
 
   /**
    * `nj.registerFilter`, register filter and expression to NornJ.
    */
-  registerFilter(options: object): void;
+  export function registerFilter(options: object): void;
 
   /**
    * `nj.registerExtension`, register tag and directive to NornJ.
    */
-  registerExtension(name: string, extension: Function, options?: object, mergeConfig?: boolean): void;
+  export function registerExtension(name: string, extension: Function, options?: object, mergeConfig?: boolean): void;
 
   /**
    * `nj.registerExtension`, register tag and directive to NornJ.
    */
-  registerExtension(options: object): void;
+  export function registerExtension(options: object): void;
 
   /**
    * `nj.taggedTmpl`, NornJ tagged templates syntax `njs`.
    */
-  taggedTmpl(strs: TemplateStringsArray, ...args: any);
+  export function taggedTmpl(strs: TemplateStringsArray, ...args: any);
 
   /**
    * `nj.taggedTmplH`, NornJ tagged templates syntax `nj` and `html`.
    */
-  taggedTmplH(strs: TemplateStringsArray, ...args: any);
+  export function taggedTmplH(strs: TemplateStringsArray, ...args: any);
 
   /**
    * `nj.template`, NornJ tagged templates syntax `t`.
    */
-  template(strs: TemplateStringsArray, ...args: any);
+  export function template(strs: TemplateStringsArray, ...args: any);
 
   /**
    * `nj.expression`, NornJ tagged templates syntax `n`.
    */
-  expression(strs: TemplateStringsArray, ...args: any);
+  export function expression(strs: TemplateStringsArray, ...args: any);
 
   /**
    * `nj.css`, NornJ tagged templates syntax `s`.
    */
-  css(strs: TemplateStringsArray, ...args: any);
+  export function css(strs: TemplateStringsArray, ...args: any);
 
   /**
    * `nj.components`.
    */
-  components: object;
+  export const components: object;
 
   /**
    * `nj.componentConfig`.
    */
-  componentConfig: object;
+  export const componentConfig: object;
 
   /**
    * `nj.filters`.
    */
-  filters: object;
+  export const filters: object;
 
   /**
    * `nj.filterConfig`.
    */
-  filterConfig: FilterConfig;
+  export const filterConfig: FilterConfig;
 
   /**
    * `nj.extensions`.
    */
-  extensions: object;
+  export const extensions: object;
 
   /**
    * `nj.extensionConfig`.
    */
-  extensionConfig: object;
+  export const extensionConfig: object;
 }
