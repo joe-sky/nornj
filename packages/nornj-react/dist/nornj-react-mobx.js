65490bc69d4f9c0134af6fd86d262aaa4e9f00c9
joe-sky <hitomi_doax@sina.com>
2019-11-20 19:19:11 +0800

    refactor: :label: Update typings

--- packages/nornj-react/dist/nornj-react-mobx.js
+++ packages/nornj-react/dist/nornj-react-mobx.js
@@ -1,5 +1,5 @@
 /*!
-* NornJ-React-Mobx v5.0.0-rc-47
+* NornJ-React-Mobx v5.0.0-rc.49
 * (c) 2016-2019 Joe_Sky
 * Released under the MIT License.
 */
--- packages/nornj-react/typings/global.d.ts
+++ packages/nornj-react/typings/global.d.ts
@@ -1,5 +1,15 @@
+/**
+ * NornJ tag `MobxObserver` is the same as the Observer component of Mobx(https://mobx-react.js.org/observer-component), but its children does not need a nested function. example:
+ *
+ * `<MobxObserver><div>{store.foo}</div></MobxObserver>`
+ */
 declare function MobxObserver(): any;
 
+/**
+ * NornJ tag `MobxObserver` is the same as Observer component of Mobx(https://mobx-react.js.org/observer-component), but its children does not need a nested function. example:
+ *
+ * `<NjMobxObserver><div>{store.foo}</div></NjMobxObserver>`
+ */
 declare function NjMobxObserver(): any;
 
 declare namespace JSX {
@@ -12,8 +22,18 @@ declare namespace JSX {
   }
 
   interface IntrinsicElements {
+    /**
+     * NornJ tag `mobxObserver` is the same as the Observer component of Mobx(https://mobx-react.js.org/observer-component), but its children does not need a nested function. example:
+     *
+     * `<mobxObserver><div>{store.foo}</div></mobxObserver>`
+     */
     mobxObserver: NornJReact.MobxObserver;
 
+    /**
+     * NornJ tag `mobxObserver` is the same as the Observer component of Mobx(https://mobx-react.js.org/observer-component), but its children does not need a nested function. example:
+     *
+     * `<n-mobxObserver><div>{store.foo}</div></n-mobxObserver>`
+     */
     'n-mobxObserver': NornJReact.MobxObserver;
   }
 }
